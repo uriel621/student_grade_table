a2e57f6045d818f19b23c4896fa03f25e7021532
Ever Garcia <uriel621@live.com>
2017-02-25 20:59:39 -0800

    add

--- 1.5/js/script.js
+++ 1.5/js/script.js
@@ -1,23 +1,77 @@
 var student_grade_table = (function(){
     
+
+    var studentInfo = [];
     var actions = {
         
         
         add: function(){
-            $("#add").click(function(){
-                alert("Add");
+            $("#add").click(function(name, course, grade){
+                // alert("Add");
 
-                var add = $("<div>", {
-                    text: "Name"
-                })
-                $("tbody").append(add)
+                var info = {
+                    name: $("#studentName").val(),
+                    course: $("#course").val(),
+                    grade: $("#studentGrade").val(),
+                }
+                
+                var pushesInfoToStudentInfo = function(){
+                    studentInfo.push(info)
+                    console.log(studentInfo)
+                };
+
+                var clearInputs = (function(){
+                    $("#studentName").val("");
+                    $("#course").val("");
+                    $("#studentGrade").val("");
+                }());
+                    
+                var domCreation = function(){
+                    var studentRow = $("<tr>");
+                        
+                    var studentName = $("<td>", {
+                        text: info.name
+                    });
+                    var studentCourse = $("<td>", {
+                        text: info.course
+                    });
+                    var studentGrade = $("<td>", {
+                        text: info.grade
+                    });
+                    var studentOperations = $("<td>", {
+                        text: "Delete",
+                        class: "btn btn-danger delete"
+                    });
+                        
+                    $(studentRow).append(studentName, studentCourse, studentGrade, studentOperations);
+                    $("tbody").append(studentRow);
+                }; 
+
+                var test = (function(){
+                    // pushesInfoToStudentInfo();
+                    // domCreation()
+
+                    var noName = info.name;
+                    var noCourse = info.course;
+                    var noGrade = info.grade;
+
+                    if(noName === false){
+                        alert("Student Name is missing")
+                        $()
+                    }
+                    else {
+                        alert("you are missing something")
+                    }
+                }());
+
+                
 
             });
         },
 
 
         delete: function(){
-            $("#delete").click(function(){
+            $(".delete").click(function(){
                 alert("delete");
             });
         },
@@ -37,6 +91,9 @@ var student_grade_table = (function(){
         }
     };
 
+
+            
+
     var init = {
         addStudents: actions.add(),
         deleteStudents: actions.delete(),
@@ -45,6 +102,10 @@ var student_grade_table = (function(){
     };
     
 
-
+        
+            
+        
 }());
+
+        
 
